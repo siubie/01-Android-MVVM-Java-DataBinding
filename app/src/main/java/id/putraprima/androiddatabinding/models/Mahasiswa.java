c50a81f14d9f4e5defce2c8acb2df15f9dc83712
siubie <putraprima@gmail.com>
2020-10-30 05:21:01 +0700

    Data Binding Dengan Object dan Image

--- app/src/main/java/id/putraprima/androiddatabinding/models/Mahasiswa.java
+++ app/src/main/java/id/putraprima/androiddatabinding/models/Mahasiswa.java
@@ -3,6 +3,7 @@ package id.putraprima.androiddatabinding.models;
 public class Mahasiswa {
     private String nama;
     private String nim;
+    private int image;
 
     public Mahasiswa() {
     }
@@ -10,6 +11,20 @@ public class Mahasiswa {
     public Mahasiswa(String nama, String nim) {
         this.nama = nama;
         this.nim = nim;
+    }
+
+    public Mahasiswa(String nama, String nim, int image) {
+        this.nama = nama;
+        this.nim = nim;
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
     }
 
     public String getNama() {
