042bce070165629dd0bb9cbf90f417d308d53a64
siubie <putraprima@gmail.com>
2020-10-30 05:04:51 +0700

    Data Binding Dengan Object

--- app/src/main/java/id/putraprima/androiddatabinding/MainActivity.java
+++ app/src/main/java/id/putraprima/androiddatabinding/MainActivity.java
@@ -1,28 +1,23 @@
 package id.putraprima.androiddatabinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
 import android.os.Bundle;
 import android.service.autofill.TextValueSanitizer;
 import android.widget.TextView;
 
+import id.putraprima.androiddatabinding.databinding.ActivityMainBinding;
 import id.putraprima.androiddatabinding.models.Mahasiswa;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Mahasiswa mahasiswa;
-    private TextView txtNama,txtNim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setMahasiswa();
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Mahasiswa mahasiswa = new Mahasiswa("Putra Prima Arhandi","0410630078");
+        binding.setMahasiswa(mahasiswa);
     }
 
-    public void setMahasiswa(){
-        mahasiswa = new Mahasiswa("Putra Prima Arhandi","0410630078");
-        txtNama = findViewById(R.id.txtNama);
-        txtNim = findViewById(R.id.txtNim);
-        txtNama.setText(mahasiswa.getNama());
-        txtNim.setText(mahasiswa.getNim());
-    }
 }
